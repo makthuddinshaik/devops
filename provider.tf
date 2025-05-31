74c9ec6b4a5d5d910a6c3669597d0acc3d6f1d1f
makthuddinshaik <makthuddinshaik4@gmail.com>
2025-05-31 07:26:27 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region = "us-east-1"  # Change to your desired region
 }
